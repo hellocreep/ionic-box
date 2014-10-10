24f4d1b65af2231a3d1426c1789f9c6249b14573
hellocreep <hellocreep@gmail.com>
2014-10-10 16:46:16 +0800

    add JAVA_HOME

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,15 +12,16 @@ curl -O $ANDROID_SDK
 tar -xzvf $ANDROID_SDK_FILENAME
 sudo chown -R vagrant android-sdk-linux/
 
-echo "ANDROID_HOME=~/android-sdk-linux" >> /home/vagrant/.bashrc
-echo "PATH=\$PATH:~/android-sdk-linux/tools:~/android-sdk-linux/platform-tools" >> /home/vagrant/.bashrc
+echo "export ANDROID_HOME=~/android-sdk-linux" >> /home/vagrant/.bashrc
+echo "export PATH=\$PATH:~/android-sdk-linux/tools:~/android-sdk-linux/platform-tools" >> /home/vagrant/.bashrc
+echo "export export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk-i386" >> /home/vagrant/.bashrc
 
 npm install -g cordova
 npm install -g ionic
 expect -c '
 set timeout -1   ;
 spawn /home/vagrant/android-sdk-linux/tools/android update sdk -u --all --filter platform-tool,android-19,build-tools-19.1.0
-expect { 
+expect {
     "Do you accept the license" { exp_send "y\r" ; exp_continue }
     eof
 }
